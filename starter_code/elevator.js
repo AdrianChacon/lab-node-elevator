1a448cdb98731b52cb55ada21be05d30b7fa0725
Adrián Chacón <adrianchaconmadueno@gmail.com>
2018-01-29 15:26:21 +0100

    iteracion 1?

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,28 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    var intervalID = window.setInterval(myCallback, 500);
+    function myCallback() {
+      this.update();
+    }
+  }
+  stop() { intervalID = clearInterval() }
+  update() {
+    log();
+  }
   _passengersEnter() { }
   _passengersLeave() { }
   floorUp() { }
   floorDown() { }
   call() { }
-  log() { }
+  log() {
+    console.log(`Direction: ${} | Floor: ${this.floor}`)
+  }
 }
 
 module.exports = Elevator;
